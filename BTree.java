5318e4bcd3b5bb2151dc1d5c6d3fe5e7e548567b
Don Richson Que <33984642+Draqe@users.noreply.github.com>
2017-12-01 17:42:14 +0800

    Update BTree.java

--- BTree.java
+++ BTree.java
@@ -42,6 +42,13 @@ public class Btree {
 	}
 
 	public void split(Node z) {
+		// conditions needed to be satisfied
+		// 1 root node initial splits (done)
+		// 2 child of root node splits (done)
+		// push node up if child is middle node and adjust (done)
+		// 3 root node with child splits due to child and root node full
+		// 4 parent of child splits due to child and parent full
+		// be able to adjust all parent child relationship
 		if (!z.hasParent()) {
 			z.parent = new Node(order);
 			root = z.parent;
@@ -57,15 +64,26 @@ public class Btree {
 		}
 		
 		else {
-			z.parent.add(z.key[minKeys]);
-			Node newChild = new Node(order);
-			
+			if(!z.parent.isFull()) {
+				z.parent.add(z.key[minKeys]);
+				z.key[minKeys] = null;
+				Node newChild = new Node(order);
+				for (int i = minKeys + 1; i < order; i++) {
+					newChild.add(z.key[i]);
+					z.key[i] = null;
+				}
+				z.parent.addChild(newChild);
+			}
+			else {
+				
+			}
 		}
 	}
 
 	
 	class Node {
-		int pointer;
+		int keyPointer;
+		int childPointer;
 		int numChild;
 		int numKeys;
 		int minKeys;
@@ -76,7 +94,8 @@ public class Btree {
 		public Node(int x) {
 			numChild = x;
 			numKeys = x-1;
-			pointer = 0;
+			keyPointer = 0;
+			childPointer = 2;
 			minKeys = numKeys/2;
 		}
 		
@@ -88,8 +107,8 @@ public class Btree {
 		}
 		
 		public void add(int y) {
-			key[pointer] = y;
-			pointer++;
+			key[keyPointer] = y;
+			keyPointer++;
 			Arrays.sort(key, new Comparator<Integer>() {
 				 public int compare(Integer i, Integer j) {
 					 if (i == null && j == null) {
@@ -118,6 +137,25 @@ public class Btree {
 				return false;
 			else
 				return true;
+		}
+		
+		public void addChild(Node x) {
+			child[childPointer] = x;
+			childPointer++;
+			Arrays.sort(child, new Comparator<Node>() {
+				 public int compare(Node i, Node j) {
+					 if (i == null && j == null) {
+						 return 0;
+						 }
+					 if (i == null) {
+						 return 1;
+						 }
+					 if (j == null) {
+						 return -1;
+						 }
+					 return i.key[0].compareTo(j.key[0]);
+					 }
+				 });
 		}
 		
 		 
