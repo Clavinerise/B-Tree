3c8ca23407f5e488d6ec7eabd5ccfbc3e1bd2dfe
Jasmine Chan <33885999+Clavinerise@users.noreply.github.com>
2017-12-02 20:51:08 +0800

    Update ValuesRecords.java

--- ValuesRecords.java
+++ ValuesRecords.java
@@ -3,7 +3,7 @@ import java.io.*;
 
 public class ValuesRecords {
 
-    RandomAccessFile file;
+    RandomAccessFile valfile;
     long countRecords;
     int RECORD_COUNT_OFFSET = 0;
     int valSize = 258;
@@ -13,30 +13,30 @@ public class ValuesRecords {
 
         if (!file.exists()) {
             this.countRecords = 0;
-            this.file = new RandomAccessFile(strFile, "rwd");
-            this.file.seek(RECORD_COUNT_OFFSET);
-            this.file.writeLong(this.countRecords);
+            this.valfile = new RandomAccessFile(strFile, "rwd");
+            this.valfile.seek(RECORD_COUNT_OFFSET);
+            this.valfile.writeLong(this.countRecords);
         } else {
-            this.file = new RandomAccessFile(strFile, "rwd");
-            this.file.seek(RECORD_COUNT_OFFSET);
-            this.countRecords = this.file.readLong();
+            this.valfile = new RandomAccessFile(strFile, "rwd");
+            this.valfile.seek(RECORD_COUNT_OFFSET);
+            this.countRecords = this.valfile.readLong();
         }
     }
     
     // go to the i-th value
     public void access(long i) throws IOException {
-        this.file.seek(8 + i * valSize);
+        this.valfile.seek(8 + i * valSize);
     }
     
-    // write str into the file
+    // write str into the valfile
     public void write(String str) throws IOException {
         // convert string to byte
         byte[] bArr = str.getBytes("UTF8");
         short len = bArr.size();
         // write the length of the byte array from the convert
-        file.writeShort(len);
+        valfile.writeShort(len);
         // write the byte array
-        file.write(bArr);
+        valfile.write(bArr);
         // update header to reflect the count of stored records
         countRecords++;
     }
@@ -44,11 +44,11 @@ public class ValuesRecords {
     // returns the string value
     public String readValue() throws IOException {
         // get actual size of the string to read
-        short len = file.readShort();
+        short len = valfile.readShort();
         // allocate a byte array with length = size of string
         byte[] bArr = new byte[len];
         // read byte array
-        file.read(bArr);
+        valfile.read(bArr);
         // use string contructor and specify charset name to utf8
         String message = new String(bArr, "UTF-8")
         return message;
