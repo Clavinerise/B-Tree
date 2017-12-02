fb9023dc7171ef7ca7b73086da75b7e14f29c356
Clavinerise <33885999+Clavinerise@users.noreply.github.com>
2017-12-02 08:55:22 +0800

    Update btdb.java

--- btdb.java
+++ btdb.java
@@ -1,7 +1,44 @@
+
 import java.util.*;
+import java.io.*;
 
 public class btdb {
-  public static void main(String args[]) {
-    
-  }
+    int RECORD_COUNT_OFFSET = 0;
+    long countRecords;
+    RandomAccessFile valFile;
+
+    public static void main(String args[]) throws IOException{
+        
+        Scanner in = new Scanner(System.in);
+
+        String bt = args[0];
+        String val = args[1];
+        
+        BTRecords btFile = new BTRecords(bt);
+        ValuesRecords valFile = new ValuesRecords(val);
+        
+        while(in.hasNextLine()) {
+            String instruct = in.next();
+            if(instruct.equals("insert")) {
+                int key = in.nextInt();
+                String message = in.nextLine();
+                valFile.write(message);
+                System.out.println(key + " inserted.");
+            } else if (instruct.equals("update")) {
+                int key = in.nextInt();
+                String message = in.nextLine();
+                
+                System.out.println(key + " updated.");
+            } else if (instruct.equals("select")) {
+                int key = in.nextInt();
+                
+                
+                System.out.println(key + " =>");
+            } else if (instruct.equals("exit")) {
+                System.exit(0);
+            } else {
+                System.out.println("ERROR: invalid command.");
+            }
+        }
+    }
 }
