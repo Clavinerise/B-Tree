49729a4100ba7a38dad98234647f87c96c0093d8
Don Richson Que <33984642+Draqe@users.noreply.github.com>
2017-12-06 00:55:23 +0800

    Update Btree.java

--- Btree.java
+++ Btree.java
@@ -16,12 +16,12 @@ public class Btree {
 	}
 	
 	//insert a key to the btree
-	public void insert(Node z, long key) {
+	public void insert(Node z, long key, long offset) {
 		//to use node z must always be the root node
 		//check if node has a child since we only add to the deepest layer
 		if(!z.hasChild()) {
 			//add key into node
-			z.add(key);
+			z.add(key, offset);
 			//if the node is full/exceeds the max amount of keys then split
 			if(z.isFull()) 
 				split(z);
@@ -29,8 +29,8 @@ public class Btree {
 		//if it is not the deepest layer find the deepest layer
 		else {
 			for (int i = 0; i < order; i++) {
-				if (z.key[i]==null || key < z.key[i]) {
-					insert(z.child[i], key);
+				if (z.key[i][0]==null || key < z.key[i][0]) {
+					insert(z.child[i], key, offset);
 					break;
 				}
 			}
@@ -47,8 +47,9 @@ public class Btree {
 			record++;
 			root = z.parent;
 			//add in the middle key into the root node
-			root.add(z.key[minKeys]);
-			z.key[minKeys] = null;
+			root.add(z.key[minKeys][0], z.key[minKeys][1]);
+			z.key[minKeys][0] = null;
+			z.key[minKeys][1] = null;
 			//make the original root node the child of the new root node
 			root.child[0] = z;
 			//create a new child node for the root node
@@ -56,8 +57,9 @@ public class Btree {
 			record++;
 			//move the later half of the original node to the new child 
 			for (int i = minKeys + 1; i < order; i++) {
-				child2.add(z.key[i]);
-				z.key[i] = null;
+				child2.add(z.key[i][0], z.key[i][1]);
+				z.key[i][0] = null;
+				z.key[i][1] = null;
 			}
 			//add the child into the new root node
 			root.addChild(child2);
@@ -70,15 +72,17 @@ public class Btree {
 		else {
 			//since it is not a root node, it has a parent
 			//move the middle key into the parent node
-			z.parent.add(z.key[minKeys]);
-			z.key[minKeys] = null;
+			z.parent.add(z.key[minKeys][0], z.key[minKeys][1]);
+			z.key[minKeys][0] = null;
+			z.key[minKeys][1] = null;
 			//create a new child due to the split
 			Node newChild = new Node(order, record);
 			record++;
 			//move later half of the node into the new child
 			for (int i = minKeys + 1; i < order; i++) {
-				newChild.add(z.key[i]);
-				z.key[i] = null;
+				newChild.add(z.key[i][0], z.key[i][1]);
+				z.key[i][0] = null;
+				z.key[i][1] = null;
 			}
 			//accomplish parent child relationship for the new node
 			z.parent.addChild(newChild);
@@ -107,7 +111,7 @@ public class Btree {
 	
 	public Node searchNode(Node n, long key) {
 		for (int i = 0; i < order; i++) {
-			if (n.key[i] != null && n.key[i] == key) 
+			if (n.key[i][0] != null && n.key[i][0] == key) 
 				return n;
 			else {
 				for(int j = 0; j < order+1; j++) {
@@ -128,7 +132,7 @@ public class Btree {
 		if (n != null){
 			arr[0] = n.record;
 			for (int i = 0; i < order; i++){
-				if (n.key[i] == key){
+				if (n.key[i][0] == key){
 					arr[1] = i;
 					break;
 				}
@@ -141,13 +145,62 @@ public class Btree {
 		return arr;
 	}
 	
+	public long[] takeKeys(Node n) {
+		long[] key = new long[order-1];
+		for (int i = 0; i < order-1; i++) {
+			if (n.key[i][0] != null)
+				key[i] = n.key[i][0];
+			else 
+				key[i] = -1;
+		}
+		return key;
+	}
+	
+	public long[] takeOffset(Node n) {
+		long[] key = new long[order-1];
+		for (int i = 0; i < order-1; i++) {
+			if (n.key[i][0] != null)
+				key[i] = n.key[i][1];
+			else 
+				key[i] = -1;
+		}
+		return key;
+	}
+	
+	public long[] takeChildren(Node n) {
+		long[] key = new long[order];
+		for (int i = 0; i < order; i++) {
+			if(n.child[i] != null) {
+				Integer k = n.child[i].record;
+				key[i] = new Long(k);
+			}
+			else {
+				key[i] = -1;
+			}
+		}
+		return key;
+	}
+	
+	public void rootAdder(Long[][] key, int record) {
+		Node n = new Node(order,record);
+		n.key = key;
+		root = n;
+	}
+	
+	public void childAdder(Long[][] key, Node parent, int record) {
+		Node n = new Node(order,record);
+		n.key = key;
+		n.parent = parent;
+		n.parent.addChild(n);
+	}
+	
 	public class Node {
 		int keyPointer;
 		int childPointer;
 		int numChild;
 		int numKeys;
 		int minKeys;
-		Long[] key;
+		Long[][] key;
 		Node[] child;
 		Node parent;
 		int record;
@@ -160,35 +213,36 @@ public class Btree {
 			keyPointer = 0;
 			childPointer = 1;
 			minKeys = numKeys/2;
-			key = new Long[numKeys+1];
+			key = new Long[numKeys+1][2];
 			child = new Node[numChild+1];
 			record = y;
 		}
 		
 		//check if the node is full/overloaded
 		public boolean isFull() {
-			if (key[numKeys] == null)
+			if (key[numKeys][0] == null)
 				return false;
 			else 
 				return true;
 		}
 		
 		//adds a new key and sorts the keys in order
-		public void add(long y) {
-			key[keyPointer] = y;
+		public void add(long y, long x) {
+			key[keyPointer][0] = y;
+			key[keyPointer][1] = x;
 			keyPointer++;
-			Arrays.sort(key, new Comparator<Long>() {
-				 public int compare(Long i, Long j) {
-					 if (i == null && j == null) {
+			Arrays.sort(key, new Comparator<Long[]>() {
+				 public int compare(Long[] i, Long[] j) {
+					 if (i[0] == null && j[0] == null) {
 						 return 0;
 						 }
-					 if (i == null) {
+					 if (i[0] == null) {
 						 return 1;
 						 }
-					 if (j == null) {
+					 if (j[0] == null) {
 						 return -1;
 						 }
-					 return i.compareTo(j);
+					 return Long.compare(i[0],j[0]);
 					 }
 				 });
 		}
@@ -224,7 +278,7 @@ public class Btree {
 					 if (j == null) {
 						 return -1;
 						 }
-					 return i.key[0].compareTo(j.key[0]);
+					 return i.key[0][0].compareTo(j.key[0][0]);
 					 }
 				 });
 		}
